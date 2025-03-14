8413ef24828e91062584421218d2bcb541880e54
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-14 16:07:16 -0300

    Add files via upload

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
-# Configurações da aplicação
+import os
 
-SUPABASE_URL = "https://irnyfnklimdguwkxotbw.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlybnlmbmtsaW1kZ3V3a3hvdGJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE5NzM5NzQsImV4cCI6MjA1NzU0OTk3NH0.m-bghlGXt2ZAAmKxyH_306LbMrvayHU3Bwx7NwKkn9U"
-DB_PASSWORD = "sfgsg45645fghfg"
-OPENAI_KEY = os.getenv("OPENAI_KEY")  # Carrega do ambiente
+# Carregando credenciais do ambiente (Streamlit Secrets)
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+OPENAI_KEY = os.getenv("OPENAI_KEY")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
     raise ValueError("Erro: SUPABASE_URL ou SUPABASE_KEY não estão configurados corretamente.")
+
+if not OPENAI_KEY:
+    raise ValueError("Erro: OPENAI_KEY não foi configurado no ambiente do Streamlit.")
