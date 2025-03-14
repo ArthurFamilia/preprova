d918e780869387e132103bf91906390fcd3ec299
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-14 15:48:59 -0300

    Add files via upload

--- login.py
+++ login.py
@@ -19,7 +19,7 @@ def login_page(supabase):
                 if hasattr(response, "user") and response.user:
                     st.session_state["logged_in"] = True
                     st.session_state["user_email"] = email
-                    st.experimental_rerun()
+                    st.rerun()
                 else:
                     st.error("Email ou senha incorretos.")
             except Exception as e:
