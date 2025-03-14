1d438d69e2a7c3564d7cf28718af39e3e31f4e0d
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-14 16:09:49 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
-import os
+import streamlit as st
 
-# Carregando credenciais do ambiente (Streamlit Secrets)
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-OPENAI_KEY = os.getenv("OPENAI_KEY")
+# Carregando credenciais do ambiente no Streamlit Cloud
+SUPABASE_URL = st.secrets.get("SUPABASE_URL", "")
+SUPABASE_KEY = st.secrets.get("SUPABASE_KEY", "")
+DB_PASSWORD = st.secrets.get("DB_PASSWORD", "")
+OPENAI_KEY = st.secrets.get("OPENAI_KEY", "")
 
 if not SUPABASE_URL or not SUPABASE_KEY:
-    raise ValueError("Erro: SUPABASE_URL ou SUPABASE_KEY não estão configurados corretamente.")
+    raise ValueError("Erro: SUPABASE_URL ou SUPABASE_KEY não foram encontrados nos Secrets do Streamlit Cloud.")
 
 if not OPENAI_KEY:
-    raise ValueError("Erro: OPENAI_KEY não foi configurado no ambiente do Streamlit.")
+    raise ValueError("Erro: OPENAI_KEY não foi configurado nos Secrets do Streamlit Cloud.")
