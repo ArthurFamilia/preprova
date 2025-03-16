e51fd9bfe50b8f1fb0fcd555334be7ab5e069b5d
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-16 11:58:11 -0300

    Update generate_questions.py

--- generate_questions.py
+++ generate_questions.py
@@ -11,11 +11,11 @@ openai.api_key = OPENAI_KEY
 
 def extract_text_from_pdf(pdf_url):
     """Baixa o PDF do Supabase e extrai o texto."""
-
+    
     st.write(f"📂 DEBUG - Extraindo texto do PDF: {pdf_url}")
 
     # 🔹 Obtém apenas o nome do arquivo da URL (remove o Supabase URL)
-    pdf_file_name = pdf_url.split("/")[-1]  # Exemplo: "1742136686_1742099237_teste1.pdf"
+    pdf_file_name = pdf_url.split("/")[-1]  # Exemplo: "1742136833_1742099237_teste1.pdf"
     file_path_in_bucket = f"pdfs/{pdf_file_name}"  # Caminho correto no bucket
 
     # 🔹 Aguarda 5 segundos para garantir que o Supabase processe o upload
