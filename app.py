5042779feeff7ea6226de0c887be94ba4dabb8ce
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-17 15:08:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -5,31 +5,25 @@ import login
 import home
 import upload
 import preprova
+import quiz  # Novo módulo de quiz
 
 def init_connection():
-    """Inicializa a conexão com o Supabase."""
     return create_client(SUPABASE_URL, SUPABASE_KEY)
 
 def main():
-    """Gerencia a navegação e autenticação do usuário."""
     st.set_page_config(page_title="Pre Prova Medicina", layout="wide")
 
-    # Inicializa conexão com o Supabase
     supabase = init_connection()
     
     if "logged_in" not in st.session_state:
         st.session_state["logged_in"] = False
 
-    # Se não estiver logado, exibir a tela de login
     if not st.session_state["logged_in"]:
         login.login_page()
-        return  # Evita que a sidebar apareça antes do login
+        return  
     
-    # Navegação da Sidebar
     st.sidebar.title("Navegação")
-    # menu = st.sidebar.radio("Escolha a página", ["Home", "Upload PDF", "Pré-Prova", "Sair"])
-    menu = st.sidebar.radio("Escolha a página", ["Home", "Upload PDF", "Pré-Prova", "Sair"], index=["Home", "Upload PDF", "Pré-Prova", "Sair"].index(st.session_state.get("menu", "Home")))
-
+    menu = st.sidebar.radio("Escolha a página", ["Home", "Upload PDF", "Pré-Prova", "Quiz", "Sair"])
 
     if menu == "Home":
         home.home_page()
@@ -37,6 +31,8 @@ def main():
         upload.upload_pdf()
     elif menu == "Pré-Prova":
         preprova.preprova_page()
+    elif menu == "Quiz":
+        quiz.quiz_page()
     elif menu == "Sair":
         st.session_state.clear()
         st.rerun()
