ef44fb131d2a69d9f1886a7c0244a8846c740456
rodste81 <130950821+rodste81@users.noreply.github.com>
2025-03-17 16:05:47 -0300

    Update home.py

--- home.py
+++ home.py
@@ -4,7 +4,7 @@ def home_page():
     st.sidebar.title(f"Bem-vindo, {st.session_state['user_email']}")
     st.sidebar.header("Menu")
     
-    st.title("Primeiro passo - Carregue seu arquivo para gerar a Pre Prova no menu esquerdo "Pre Prova"")
+    st.title("Primeiro passo - Carregue seu arquivo para gerar a Pre Prova no menu esquerdo Pre Prova")
     st.title("Segundo passo  - Acesse o menu Pre prova para ver a prova gerada")
     st.title("Terceiro passo - Clique em Quiz para responder a pre prova")
    
